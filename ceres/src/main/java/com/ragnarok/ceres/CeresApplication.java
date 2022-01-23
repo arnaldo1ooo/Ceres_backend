cae750ecedc3bfda09feb594324d307508cfe8a1
arnaldorcm <arnaldo1ooo95@gmail.com>
2022-01-23 10:33:10 -0300

    Spring Cache

--- ceres/src/main/java/com/ragnarok/ceres/CeresApplication.java
+++ ceres/src/main/java/com/ragnarok/ceres/CeresApplication.java
@@ -2,7 +2,9 @@ package com.ragnarok.ceres;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching	//Habilita el Spring Cache
 @SpringBootApplication
 public class CeresApplication {
 
